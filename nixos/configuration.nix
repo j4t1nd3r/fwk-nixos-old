b71409f10f5b05598716a5e8f113cbbb569b813a
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-09 13:05:43 +0100

    added pkg home-manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,7 +109,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # pkgs.home-manager
+    pkgs.home-manager
     git
     warp-terminal
     xclip

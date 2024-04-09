c43235dc1ce17086f825ba63c445aa273ee890d4
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-09 17:07:31 +0100

    revert

--- flake.nix
+++ flake.nix
@@ -4,33 +4,33 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86-64-linux";
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86-64-linux";
 
-      pkgs = import nixpkgs {
-        inherit system;
+    pkgs = import nixpkgs {
+      inherit system;
 
-        config = {
-          allowUnfree = true;
-        };
+      config = {
+        allowUnfree = true;
       };
+    };
 
-      in
-      {
-      nixosConfigurations = {
-        fwk-nixos = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs system; };
+    in
+    {
+    nixosConfigurations = {
+      fwk-nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
 
-          modules = [
-          ./nixos/configuration.nix
-          ];
-        };
+        modules = [
+        ./nixos/configuration.nix
+        ];
       };
+    };
   };
 }

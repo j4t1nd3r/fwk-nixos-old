964568bf75fece7f0fce13e5892c7b84fed9e947
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-10 11:23:05 +0100

    initial home-manager config

--- nixos/home.nix
+++ nixos/home.nix
@@ -5,11 +5,11 @@
   home.homeDirectory = "/home/jat";
   home.stateVersion = "23.11"; # Please read the comment before changing.
   home.packages = [
+     
    ];
 
-   # plain files is through 'home.file'.
   home.file = {
- 
+    
   };
 
   home.sessionVariables = {
@@ -17,4 +17,18 @@
   };
 
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName  = "Jatinder Randhawa";
+    userEmail = "j4t1nd3r@gmail.com";
+  };
+
+  programs.vscode = {
+  enable = true;
+  extensions = with pkgs.vscode-extensions; [
+    dracula-theme.theme-dracula
+    bbenoist.Nix
+  ];
+};
 }

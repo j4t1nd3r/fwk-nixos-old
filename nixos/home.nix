de38fc47d4cb213e85e616551cebd20791046ef5
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-10 14:35:26 +0100

    debugging warp

--- nixos/home.nix
+++ nixos/home.nix
@@ -17,12 +17,12 @@
 
   home.file = {
     # "~/.config/warp-terminal/user_preferences.json" = {
-    #   source = config.lib.file.mkOutOfStoreSymlink "/home/jat/nix-fwk-system/configs/warp-terminal.json";
+    #   source = config.lib.file.mkOutOfStoreSymlink "nix-fwk-system/configs/warp-terminal.json";
     # };
   };
   
   # home.file."/.config/warp-terminal/user_preferences.json".source = "./configs/warp-terminal.json";
-  home.file.".config/warp-terminal/user_preferences.json".source = "../configs/warp-terminal.json";
+  home.file.".config/warp-terminal/user_preferences.json".source = "nix-fwk-system/configs/warp-terminal.json";
 
   home.sessionVariables = {
     EDITOR = "code";

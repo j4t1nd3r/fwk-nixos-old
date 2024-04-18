26c7e56e8ca0cbba9cd270b3bf2161b74eb13114
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-18 13:03:16 +0100

    home.file comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -28,8 +28,8 @@
     ];
 
     file = {
-      ".config/warp-terminal/user_preferences.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-fwk-system//configs/warp-terminal.json";
-      ".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-fwk-system/configs/vscode-settings.json";
+      ".config/warp-terminal/user_preferences.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-fwk-system//configs/warp-terminal.json"; # warp terminal
+      ".config/Code/User/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-fwk-system/configs/vscode-settings.json"; # vscode settings.json
        
     };
 

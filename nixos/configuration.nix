cacd0f0325690cbb94e14ae3f593834e131448bf
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-21 14:14:18 +0100

    nix.nixPath

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,7 +21,7 @@
   };
 
   nix.nixPath = [
-    "nixpkgs=flake:nixpkgs:/nix/var/nix/profiles/per-user/root/channels/nixos"
+    "nixpkgs=flake:nixpkgs:/nix/var/nix/profiles/per-user/root/channels"
     "nixos-config=/home/jat/nix-fwk-system/nixos/configuration.nix"
     "/nix/var/nix/profiles/per-user/root/channels"
   ];

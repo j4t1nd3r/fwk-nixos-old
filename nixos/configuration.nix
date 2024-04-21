f01d7958040051809e3c3732493b185f7855d3cb
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-21 15:58:40 +0100

    programs.hyprland.enable = true;

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -108,7 +108,8 @@
     pkgs.home-manager
   #  wget
   ];
-
+    programs.hyprland.enable = true;
+    
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

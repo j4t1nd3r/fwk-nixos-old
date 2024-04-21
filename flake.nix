7cea64b52c0884e1ad27ec5a1f9bdaa86fa7e225
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-21 12:25:36 +0100

    switched flake to unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "nixos flake config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {

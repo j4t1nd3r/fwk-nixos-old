8666220cdcb04ecdbc27232fbe637618fd88cec0
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-25 21:52:51 +0100

    debug sddm theme

--- modules/sddm-theme.nix
+++ modules/sddm-theme.nix
@@ -19,8 +19,5 @@ pkgs.stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out
     cp -R ./* $out/
-    # cd $out/
-    # rm Background.jpg
-    # cp -r ${image} $out/Background.jpg
    '';
 }

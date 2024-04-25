7009842501764e9738ea269c1f42eef21947ea60
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-25 21:36:35 +0100

    debug sddm theme

--- modules/sddm-theme.nix
+++ modules/sddm-theme.nix
@@ -5,7 +5,7 @@ let
 
   image = pkgs.fetchurl {
     url = imgLink;
-    sha256 = "sha256-HrcYriKliK2QN02/2vFK/osFjTT1NamhGKik3tozGU0=";
+    sha256 = "1qyq05ghxvfncs873jmw4lzf8sl3rg3502zgvangqcj7mdqb02zm";
   };
 in
 pkgs.stdenv.mkDerivation {

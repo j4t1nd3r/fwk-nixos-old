81e3243397ea8a2cc1b373b7b66343efb53b0eec
Jatinder Randhawa <j4t1nd3r@gmail.com>
2024-04-30 16:19:28 +0100

    hyprland removal

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -37,35 +37,6 @@
     };
   };
 
-  # wayland.windowManager.hyprland = {
-  #   enable = true;
-  #   settings = {
-  #       "$mod" = "SUPER";
-  #       bind =
-  #         [
-  #           "$mod, F, exec, firefox"
-  #           "$mod, W, exec, warp-terminal"
-  #           ", Print, exec, grimblast copy area"
-  #         ]
-  #         ++ (
-  #           # workspaces
-  #           # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-  #           builtins.concatLists (builtins.genList (
-  #               x: let
-  #                 ws = let
-  #                   c = (x + 1) / 10;
-  #                 in
-  #                   builtins.toString (x + 1 - (c * 10));
-  #               in [
-  #                 "$mod, ${ws}, workspace, ${toString (x + 1)}"
-  #                 "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-  #               ]
-  #             )
-  #             10)
-  #         );
-  #     };
-  # };
-
   # set allowunFree
   nixpkgs = {
     config = {
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -71,14 +71,6 @@
   };
   services.desktopManager.plasma6.enable = true;
 
-  # hyprland config
-  # programs.hyprland.enable = true;
-  
-  # nix.settings = {
-  #   substituters = ["https://hyprland.cachix.org"];
-  #   trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  # };
-
   # kwallet - avoid chrome prompt on each login
   security.pam.services.kdwallet.kwallet.enable = true; 
   
